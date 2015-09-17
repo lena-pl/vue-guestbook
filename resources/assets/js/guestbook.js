7f381a01b893ed9f045f195e5ea2c4503e98dbe8
Lena Plaksina <lena.plaksina@gmail.com>
2015-09-18 10:34:56 +1200

    Remove build files from repo, add fetchMessages function

--- resources/assets/js/guestbook.js
+++ resources/assets/js/guestbook.js
@@ -1,5 +1,17 @@
 new Vue({
 
-	el: '#guestbook'
+	el: '#guestbook',
+
+	ready: function() {
+		this.fetchMessages();
+	},
+
+	methods: {
+		fetchMessages: function() {
+			this.$http.get('/api/messages', function(messages) {
+				this.$set('messages', messages);
+			});
+		}
+	}
 
 });

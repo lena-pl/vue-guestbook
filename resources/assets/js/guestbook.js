c625649ad9033eb6f0c9b0c4886fef922cb873d4
Lena Plaksina <lena.plaksina@gmail.com>
2015-09-19 21:57:38 +1200

    Add onSubmitForm function and submitted data. Started work on CSRF token.

--- resources/assets/js/guestbook.js
+++ resources/assets/js/guestbook.js
@@ -1,3 +1,5 @@
+Vue.http.headers.common['X-CSRF-TOKEN'] = document.querySelector('#token').getAttribute('value');
+
 new Vue({
 
 	el: '#guestbook',
@@ -6,7 +8,8 @@ new Vue({
 		newMessage: {
 			name: '',
 			message: ''
-		}
+		},
+		submitted: false
 	},
 
 	computed: {
@@ -28,6 +31,27 @@ new Vue({
 			this.$http.get('/api/messages', function(messages) {
 				this.$set('messages', messages);
 			});
+		},
+
+		onSubmitForm: function() {
+
+			// prevent the default action
+			e.preventDefault();
+
+			// save variable for the message object
+			var message = this.newMessage;
+
+			// add new message to "messages" array
+			this.messages.push(message);
+
+			// reset input values
+			this.newMessage = {name: '', message: ''};
+			
+			// show thanks message
+			this.submitted = true;
+
+			// send POST ajax request
+			this.$http.post('api/messages', message);
 		}
 	}
 
--- resources/views/guestbook.blade.php
+++ resources/views/guestbook.blade.php
@@ -3,6 +3,7 @@
 
   <head>
     <meta charset="utf-8">
+    <meta id="token" name="token" value="{{ csrf_token() }}">
     <title>Vue and Laravel Powered Guestbook</title>
 
     <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
@@ -17,18 +18,26 @@
 
         <form method="POST" v-on="submit: onSubmitForm">
           <div class="form-group">
-            <label for="name">Name:</label>
+            <label for="name">
+              Name:
+              <span class="error" v-if="! newMessage.name"> *</span>
+            </label>
             <input type="text" name="name" id="name" class="form-control" v-model="newMessage.name">
           </div>
 
           <div class="form-group">
-            <label for="message">Message:</label>
+            <label for="message">
+              Message:
+              <span class="error" v-if="! newMessage.message"> *</span>
+            </label>
             <textarea type="text" name="message" id="message" class="form-control" v-model="newMessage.message"></textarea>
           </div>
 
-          <div class="form-group">
+          <div class="form-group" v-if="! submitted">
             <button type="submit" class="btn btn-default" v-attr="disabled: errors">Sign Guestbook</button>
           </div>
+
+          <div class="alert alert-success" v-if="submitted">Thanks!</div>
         </form>
 
         <hr>
